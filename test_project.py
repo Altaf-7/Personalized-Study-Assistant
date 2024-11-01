31e84e69f7ac43be1b7aa8f2507ff13dd0b4c759
Altaf-7 <altafraja010789@gmail.com>
2024-11-01 11:20:09 +0530

    wrote test for first two fn

--- test_project.py
+++ test_project.py
@@ -3,39 +3,56 @@ from datetime import date
 import pytest
 
 
+# hours_per_day and total_days are getting checked for correction in main(), so no need to check here
 def test_schedule_study():
-    assert schedule_study(["chem", "phy", "maths"], 3, date(2024, 11, 1)) == {
-        "chem": 1.0,
-        "phy": 1.0,
-        "maths": 1.0,
+    assert schedule_study({"chem": 4, "phy": 3, "maths": 2}, 6.5, 45) == {
+        "chem": "2:53",
+        "phy": "2:10",
+        "maths": "1:26",
     }
 
-    assert schedule_study(["a", "b", "c", "d"], 8, date(2024, 12, 1)) == {
-        "a": 2.0,
-        "b": 2.0,
-        "c": 2.0,
-        "d": 2.0,
+    assert schedule_study({"a": 3, "b": 3, "c": 3, "d": 3}, 8, 100) == {
+        "a": "2:00",
+        "b": "2:00",
+        "c": "2:00",
+        "d": "2:00",
     }
 
-    assert schedule_study(["a", "b", "c"], 4, date(2025, 2, 10)) == {
-        "a": 1.33,
-        "b": 1.33,
-        "c": 1.33,
+    assert schedule_study({"a": 4, "b": 3, "c": 1}, 4.0, 15) == {
+        "a": "2:00",
+        "b": "1:30",
+        "c": "0:30",
     }
 
     assert schedule_study(
-        ["matrix", "circle", "vectors", "probability"], 2, date(2025, 6, 28)
-    ) == {"matrix": 0.5, "circle": 0.5, "vectors": 0.5, "probability": 0.5}
+        {"matrix": 5, "circle": 3, "vectors": 4, "probability": 5}, 2.5, 5
+    ) == {"matrix": "0:44", "circle": "0:26", "vectors": "0:35", "probability": "0:44"}
 
-    with pytest.raises(SystemExit):
-        schedule_study(["A", "B"], 4, date(2024, 10, 1))
-        schedule_study(["m", "n"], 6, date(2024, 10, 29))
-
-    ...  # add more edge cases like hrs_can_you_study = 0 and others
+    assert schedule_study(
+        {"matrix": 2, "circle": 1, "vectors": 3, "probability": 4}, 0, 3
+    ) == {"matrix": "0:00", "circle": "0:00", "vectors": "0:00", "probability": "0:00"}
 
 
-# def test_track_progress():
-#     ...
+# topic and time_studied are getting checked for correction in main(), so no need to check here
+def test_track_progress():
+    assert track_progress(
+        {"chem": "2:53", "phy": "2:10", "maths": "1:26"}, "chem", "2:00"
+    ) == {"chem": "0:53", "phy": "2:10", "maths": "1:26"}
+
+    assert track_progress(
+        {"algebra": "4:00","statitics": "3:30", "complex": "1:15", "linear equation": "2:20",}, "linear equation","1:45"
+    ) == {"algebra": "4:00", "statitics": "3:30", "complex": "1:15", "linear equation": "0:35"}
+
+    assert track_progress({"a": "2:00", "b": "1:30", "c": "0:30"}, "c", "1:15") == {
+        "a": "2:00", "b": "1:30", "c": "0:00",}
+
+    assert track_progress(
+        {"matrix": "1:20", "circle": "2:15", "vectors": "3:55", "probability": "2:35"},
+        "vectors","0:00") == {"matrix": "1:20", "circle": "2:15", "vectors": "3:55", "probability": "2:35"}
+    
+    assert track_progress(
+        {"loops": "2:25", "functions": "1:55", "exception handling": "0:55", "classes & objects": "1:35"},
+        "loops","2:25") == {"loops": "0:00", "functions": "1:55", "exception handling": "0:55", "classes & objects": "1:35"}
 
 
 # def test_generate_report():
